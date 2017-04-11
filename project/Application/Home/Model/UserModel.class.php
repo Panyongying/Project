277a0f7b3b816bb5af2f23b66db3c5830f5ac7b1
zwb <zwb@qq.com>
2017-04-11 22:36:28 +0800

    一部分个人中心，新增登录注册页面

--- project/Application/Home/Model/UserModel.class.php
+++ project/Application/Home/Model/UserModel.class.php
@@ -42,6 +42,7 @@
 
 			array('termsAndConditions', 'on', '请阅读隐私政策',1, 'equal'),
 
+
 			 
 		);
 
@@ -243,5 +244,56 @@
 			}
 		}
 
+		//激活邮箱
+		public function activation()
+		{
+			$mail = new \Org\Util\mailer\PHPMailer;
+			$email = I('get.email');
+			$inner = md5(mt_rand(0,999));
+			$time = time();
+			$mail->SMTPDebug = false;
+			$mail->isSMTP();
+			$mail->SMTPAuth=true;
+			$mail->Host = 'smtp.qq.com';
+			$mail->SMTPSecure = 'ssl';
+			//设置ssl连接smtp服务器的远程服务器端口号 可选465或587
+			$mail->Port = 465;
+			$mail->Hostname = 'localhost';
+			$mail->CharSet = 'UTF-8';
+			$mail->FromName = 'H&M';//寄件人名
+			$mail->Username ='472671496';//smtp邮件服务器
+			$mail->Password = 'ngszqfcsinfjcbdg';//stmp连接认证不用改
+			$mail->From = '472671496@qq.com';//发件人
+			$mail->isHTML(true); 
+			$mail->addAddress($email,'');//收件人
+			$mail->Subject = '激活H&M邮箱';//主题
+			$mail->Body = "请点链接激活你的邮箱"."<a href='http://127.0.0.1/obj-4.6/Project/project/index.php/Home/User/register/mail/$email/time/$time'>".$inner."</a>";
+			$status = $mail->send();			
+			
+			return $status;
+		}
 
+		//个人中心
+		
+		public function showPersonal()
+		{
+			$id = I('get.id');
+
+			$res = M('user')->field('hm_user.id,hm_user.email,hm_user.addtime,hm_account.birthday,hm_account.nickname,hm_account.sex,hm_account.phone')->join('left join hm_account ON hm_account.uid = hm_user.id')->where("hm_user.id=$id")->find();
+
+
+			$res['addtime'] = date('Y-m-d H:i:s',$res['addtime']);
+
+			$arr = ['小姐','先生','保密'];
+
+			$res['sex'] = $arr[$res['sex']];
+			
+			$res['birthyear'] = substr($res['birthday'],0,4);
+
+			$res['birthmonth'] = substr($res['birthday'],4,2);
+
+			$res['birthday'] = substr($res['birthday'],6);
+
+			return $res;
+		}
 	}
