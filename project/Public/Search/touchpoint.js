75e09a98e9f54ad77d785ee785c511b2787ee0d0
van19960118 <542495248@qq.com>
2017-04-17 10:26:50 +0800

    恢复回滚的影响

--- project/Public/Search/touchpoint.js
+++
@@ -1,1 +0,0 @@
-function getTouchpoint(){ return 'DESKTOP';} function getTealiumURL(countryCode){ var url = '//tags.tiqcdn.com/utag/hm/cn/prod/utag.js'; if (countryCode) { url = url.replace('${country}', countryCode.toLowerCase())}; return url;}
